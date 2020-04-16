aaedd51c6437d19ed92920ba7d71cb95a04a3907
nadiaerinnar <nadiaerahmawati@gmail.com>
2020-04-16 18:25:36 +0700

    co

---
+++ app/src/main/java/com/example/coroutineroom/NoteDao.kt
@@ -0,0 +1,25 @@
+package com.example.coroutineroom
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.coroutineroom.catatan.Note
+
+@Dao
+interface NoteDao {
+
+    @Insert
+    fun insert(note: Note)
+
+    @Update
+    fun update(note: Note)
+
+    @Delete
+    fun delete(note: Note)
+
+    @Query("DELETE FROM note_table")
+    fun deleteAllNotes()
+
+    @Query("SELECT * FROM note_table ORDER BY id DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+}
